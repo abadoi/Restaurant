d2896552367b28f4b6edcbe49cac7314bd16b190
danutrusu <danutmihaitarusu@gmail.com>
2016-03-04 13:40:44 +0100

    Login page with user and password.

--- src/main/java/com.andrei.restaurant/Application.java
+++ src/main/java/com.andrei.restaurant/Application.java
@@ -1,35 +1,13 @@
 package com.andrei.restaurant;
 
-
-
 import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-        import org.springframework.context.annotation.*;
-        import org.springframework.web.bind.annotation.PathVariable;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
-@ComponentScan
-@Configuration
-
+@SpringBootApplication
 public class Application {
 
-    @Bean
-    MessageService mockMessageService() {
-        return () -> "Hello World!";
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class);
-
-    }
-}
-@RestController
-class GreetingController {
-
-    @RequestMapping
-    String hello() {
-        return "Hello, !:P";
-    }
 }
--- src/main/java/com.andrei.restaurant/MessageService.java
+++
@@ -1,9 +0,0 @@
-package com.andrei.restaurant;
-
-/**
- * Created by andreibadoi on 29/02/16.
- */
-public interface MessageService {
-    String getMessage();
-}
-
